37d3eee0e8759f319cbe19340e2c4fd63debd857
Mason Kohls <mason.kohls1999@gmail.com>
2020-04-30 13:14:47 -0700

    Refactored search code into .py

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 from astropy.coordinates import EarthLocation, Longitude, Latitude
 import astropy.units as u
+import numpy as np
 
 
 def get_earth_grid(n):
@@ -7,9 +8,35 @@ def get_earth_grid(n):
     for i in range(0, n):
         for j in range(0, n):
             earth_loc = EarthLocation.from_geodetic(Longitude(j * 360 / n - 180, u.degree),
-                                                    Latitude(i * 180 / n - 90, u.degree))
+                                                    Latitude(i * 180 / n - 90 + 180 / n / 2, u.degree))
             grid.set(earth_loc, i, j)
     return grid
+
+
+def nearest(trial_sols, observation):
+    def center(array):
+        tmp = sum(~np.isnan(array))
+        if tmp:
+            average = np.nansum(array) / sum(~np.isnan(array))
+            return array-average
+        return array
+
+    obs = center(observation)
+    n = 20
+    errors = [0] * len(trial_sols)
+
+    for i in range(0, n ** 2):
+        result = center(trial_sols[i]) - obs
+        tmp = sum(~np.isnan(result))
+        if tmp:
+            result = (result ** 2) / tmp
+        err = np.nansum(result).value
+        not_matched = sum(np.isnan(obs) * (~np.isnan(trial_sols[i]))) + sum((~np.isnan(obs)) * np.isnan(trial_sols[i]))
+        if not_matched >= 2:
+            err = np.inf
+        errors[i] = err
+    indices = np.argsort(errors)
+    return indices
 
 
 class Grid:
@@ -31,7 +58,8 @@ class Grid:
     def set(self, val, x, y=None):
         if y is not None:
             self.lst[self.getIndex(x, y)] = val
-        self.lst[x] = val
+        else:
+            self.lst[x] = val
 
     def getAdjacent(self, x, y):
-        return [self.getIndex(v_x, v_y) for v_x, v_y in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]]
+        return [self.getIndex(v_x, v_y) for v_x, v_y in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]]
